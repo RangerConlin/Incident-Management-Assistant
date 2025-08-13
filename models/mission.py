dae5092632a6f22a768e2809f5385bf715fec801
RangerConlin <pheleyb@gmail.com>
2025-08-13 03:05:50 -0400

    Convert Team, Task, and Mission to dataclasses

--- models/mission.py
+++ models/mission.py
@@ -1,15 +1,20 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
 class Mission:
-    def __init__(self, id, number, name, type, description, status, icp_location, start_time, end_time, is_training):
-        self.id = id
-        self.number = number
-        self.name = name
-        self.type = type  # e.g., "Search and Rescue", "Disaster Relief"
-        self.description = description
-        self.status = status  # e.g., "Active", "Closed", etc.
-        self.icp_location = icp_location
-        self.start_time = start_time
-        self.end_time = end_time
-        self.is_training = is_training  # Boolean
+    id: Optional[int]
+    number: str
+    name: str
+    type: str  # e.g., "Search and Rescue", "Disaster Relief"
+    description: str
+    status: str  # e.g., "Active", "Closed", etc.
+    icp_location: str
+    start_time: Optional[str]
+    end_time: Optional[str]
+    is_training: bool  # Boolean
 
     def __str__(self):
         return f"{self.name} ({self.status})"
+
