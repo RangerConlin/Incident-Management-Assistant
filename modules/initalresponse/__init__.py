5726c53194a80d3b78c637b73970d83f8d356357
RangerConlin <pheleyb@gmail.com>
2025-08-25 23:53:24 -0400

    Delete modules/initalresponse directory

--- modules/initalresponse/__init__.py
+++
@@ -1,4 +0,0 @@
-from modules.initialresponse import windows as initial
-from modules.initialresponse import get_hasty_panel, get_reflex_panel
-
-__all__ = ["initial", "get_hasty_panel", "get_reflex_panel"]
