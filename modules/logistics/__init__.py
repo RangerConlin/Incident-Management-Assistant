67e7b57bac8ddcca9598261a40e9d36fc62310ec
RangerConlin <pheleyb@gmail.com>
2025-08-29 02:24:58 -0400

    Remove get_requests_panel from logistics module

--- modules/logistics/__init__.py
+++ modules/logistics/__init__.py
@@ -1,7 +1,6 @@
 from .windows import (
     get_logistics_panel,
     get_checkin_panel,
-    get_requests_panel,
     get_equipment_panel,
     get_213rr_panel,
     get_personnel_panel,
@@ -11,7 +10,6 @@ from .windows import (
 __all__ = [
     "get_logistics_panel",
     "get_checkin_panel",
-    "get_requests_panel",
     "get_equipment_panel",
     "get_213rr_panel",
     "get_personnel_panel",
