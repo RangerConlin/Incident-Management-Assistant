003c3332fb0827296853679f5f885e9c467131ce
RangerConlin <pheleyb@gmail.com>
2025-09-04 04:24:01 -0400

    chore: remove unused scripts

--- _schema_dump.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-con=sqlite3.connect('data/master.db')
-cur=con.cursor()
-for name,sql in cur.execute("SELECT name, sql FROM sqlite_master WHERE type='table';").fetchall():
-    print(name)
-    print(sql or '')
-con.close()
